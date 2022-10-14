c6b6973af20c725d09340b7e922aeb45bc764a13
vasilii_tarasov <tarasov_vas@mail.ru>
2022-10-14 11:49:49 +0300

    commit

--- homeworks/chapter-2/1_A.py
+++ homeworks/chapter-2/1_A.py
@@ -1,1 +1,9 @@
-print("hello world")
+def greetings(name, thurname):
+    print("Доброго времени суток," + name + " Человек " + thurname + "!")
+
+fullname = input().split(" ")
+name = fullname[0]
+thurname = fullname[1]
+
+print(greetings(name, thurname))
+
