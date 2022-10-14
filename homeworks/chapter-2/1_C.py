c8b7c6e7bedff0f542eaa02327074c0da9d5fb31
Vasilii Tarasov <111644003+pendoss@users.noreply.github.com>
2022-10-14 19:30:48 +0300

    fix

--- homeworks/chapter-2/1_C.py
+++ homeworks/chapter-2/1_C.py
@@ -1,9 +1,11 @@
 def summation(num):
-    m = max(num)
+    
     s = 0
     for i in range(0,len(num)):
         if num[i] < 0:
-            num[i] = abs(num[i])
+            num[i] = abs(num[i]) * 2
+    m = max(num)
+    for i in range(0,len(num)):
         num[i] = num[i] / m
         s+=num[i]
     return s
